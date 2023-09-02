a57ad9c8494289a3d7d559584992fe487260e321
KingKazmaa <114176897+KingKazmaa@users.noreply.github.com>
2023-09-02 16:28:59 -0500

    Update Number Guessing Game_gg.py

--- Number Guessing Game_gg.py
+++ Number Guessing Game_gg.py
@@ -1,20 +1,40 @@
 import random
 import math
 
-computer_Guess = random.randint(1,50)
-user_Guess = int(input('enter a number between 1 and 50: '))
+MIN_GUESS = 1
+MAX_GUESS = 50
 
-while user_Guess!=computer_Guess:
-    if user_Guess > computer_Guess:
-        print('Lmao higher than Wiz Khalifa in April')
-        user_Guess = int(input('enter a number between 1 and 50: '))
-    elif user_Guess < computer_Guess:
-        print('Lmao lower than my gpa')
-        user_Guess = int(input('enter a number between 1 and 50: '))
-    elif user_Guess > 50 or user_Guess < 1:
-        print('come on mane you read the prompt be fr...')
-        user_Guess = int(input('enter a number between 1 and 50: '))
+def get_user_guess():
+    while True:
+        user_guess = input('enter a number between 1 and 50: ')
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+            if user_guess > 0:
+                break
+            else:
+                print('Number must be greater than 0.')
+        else:
+            print('quit playin, a real number')
+    return user_guess
 
-if user_Guess==computer_Guess:
-    print('You Win')
-    print('You get nothing now go away!')
+def compare_nums():
+    computer_guess = random.randint(MAX_GUESS)
+    
+    while user_guess != computer_guess:
+        if user_guess > computer_guess:
+            print('Lmao higher than Wiz Khalifa in April')
+            get_user_guess()
+        elif user_guess < computer_guess:
+            print('Lmao lower than my gpa')
+            get_user_guess()
+        elif MIN_GUESS > user_guess > MAX_GUESS:
+            print('come on mane you read the prompt be fr...')
+            get_user_guess()
+        else:
+            print('You Win')
+            print('You get nothing now go away!')
+            break
+
+def play_game():
+    get_user_guess()
+    compare_nums()
